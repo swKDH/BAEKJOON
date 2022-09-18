7fdef1b3b9b940761e8b991b4b13d2782cab27e3
Kang Dong Hyun <39311129+swKDH@users.noreply.github.com>
2022-09-18 20:20:01 +0900

    Add files via upload

---
+++ bj10171.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+public class bj10171 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println( "\\    /\\\n" + 
+				" )  ( ')\n" + 
+				"(  /  )\n" + 
+				" \\(__)|");
+	}
+
+}
